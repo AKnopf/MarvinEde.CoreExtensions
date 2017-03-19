4a4584045b77bd30449e680bd59232defc6f4847
Marvin Ede <marvinede@gmx.net>
2017-03-19 18:27:09 +0100

    Adds String#ToEnum and String#SearchEnum

--- CoreExtensions/String.cs
+++ CoreExtensions/String.cs
@@ -1,4 +1,8 @@
-﻿namespace MarvinEde.CoreExtensions
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MarvinEde.CoreExtensions
 {
     /// <summary>
     /// Static class that holds all extension methods for <see cref="System.String"/>
@@ -26,6 +30,59 @@
         public static bool IsPresent(this string @string)
         {
             return !@string.IsBlank();
+        }
+
+        /// <summary>
+        /// Finds an Enum value by its name. Ignores casing. Throws ArgumentException when T is not an enum
+        /// 
+        /// Example:
+        /// <code>
+        /// enum Fruit { Apple, Peach, Melon }
+        /// "Apple".ToEnum&lt;Fruit&gt;() // Fruit.Apple
+        /// "apple".ToEnum&lt;Fruit&gt;() // Fruit.Apple
+        /// </code>
+        /// 
+        /// </summary>
+        /// <typeparam name="T">Enum</typeparam>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static T ToEnum<T>(this string description) where T : struct, IConvertible
+        {
+            Type type = typeof(T);
+            if (!type.GetTypeInfo().IsEnum)
+                throw new ArgumentException("T must be an enum");
+
+            foreach (T values in Enum.GetValues(type))
+            {
+                FieldInfo field = type.GetField(Enum.GetName(type, values));
+                if (string.Equals(description, Enum.GetName(type, values), StringComparison.OrdinalIgnoreCase))
+                    return values;
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Returns all enums where one of the international descriptions contains the given term.
+        /// Default is case sensitive.
+        /// </summary>
+        public static IEnumerable<T> SearchEnum<T>(this string @string, bool ignoreCase = false) where T : struct, IConvertible
+        {
+            Type type = typeof(T);
+            if (!type.GetTypeInfo().IsEnum)
+                throw new ArgumentException("T must be an enum");
+
+            foreach (T item in Enum.GetValues(type))
+            {
+                FieldInfo field = type.GetField(Enum.GetName(type, item));
+
+                bool nameContainsTerm = ignoreCase
+                        ? Enum.GetName(type, item).ToLower().Contains(@string.ToLower())
+                        : Enum.GetName(type, item).Contains(@string);
+
+                if (nameContainsTerm)
+                    yield return item;
+            }
+            yield break;
         }
     }
 }
---
+++ CoreExtensionsTest/String.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MarvinEde.CoreExtensions;
+using System.Linq;
+
+namespace CoreExtensionsTest
+{
+    [TestClass]
+    public class String
+    {
+        private enum Fruit {  Apple, Peach, Melon }
+        private enum Characters { Mario, Bowser, Peach }
+        [TestMethod]
+        public void TestToEnum()
+        {
+            Assert.AreEqual(Fruit.Peach, "Peach".ToEnum<Fruit>());
+            Assert.AreEqual(Characters.Peach, "Peach".ToEnum<Characters>());
+        }
+
+        [TestMethod]
+        public void TestToEnumDefault()
+        {
+            Assert.AreEqual(Characters.Mario, "Apple".ToEnum<Characters>());
+        }
+
+        [TestMethod]
+        public void TestSearchEnum()
+        {
+            var actual = "r".SearchEnum<Characters>().ToList();
+            Assert.AreEqual(Characters.Mario, actual[0]);
+            Assert.AreEqual(Characters.Bowser, actual[1]);
+        }
+
+        [TestMethod]
+        public void TestSearchEnumCaseSensitive()
+        {
+            Assert.IsTrue("R".SearchEnum<Characters>(true).Any());
+            Assert.IsFalse("R".SearchEnum<Characters>(false).Any());
+        }
+    }
+}
